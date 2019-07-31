5b930d82d72d1a3377c0496e5b238a5227dc2421
wkaanig <15670813638@163.com>
2019-07-31 11:15:54 +0800

    更改设置

--- app/config/secure.py
+++ app/config/secure.py
@@ -4,6 +4,7 @@
 """
 from app.config.setting import BaseConfig
 
+
 class DevelopmentSecure(BaseConfig):
     """
     开发环境安全性配置
@@ -25,16 +26,16 @@ class ProductionSecure(BaseConfig):
 
     SECRET_KEY = '\x88W\xf09\x91\x07\x98\x89\x87\x96\xa0A\xc68\xf9\xecJJU\x17\xc5V\xbe\x8b\xef\xd7\xd8\xd3\xe6\x95*4'
 
+
 WX_APP = {
     'appid': 'wx37c9d625eddac654',
     'app_secret': '8d11a05dc9127dd9cfd23e28e03bba2b',
     'paykey': '02CBCE018C664DEB4462AB314B710193',
     'mch_id': '1531633161',
     'callback_url': '/wx/pay/callback',
-    'refund_url':'/wx/pay/rcallback'
+    'refund_url': '/wx/pay/rcallback'
 }
 
 APP = {
     'domain': 'http://flask.wkaanig.cn'
 }
-
--- app/config/setting.py
+++ app/config/setting.py
@@ -5,6 +5,7 @@
 
 from datetime import timedelta
 
+
 class BaseConfig(object):
     """
     基础配置
@@ -14,11 +15,11 @@ class BaseConfig(object):
     PAGE_DEFAULT = 0
     PAGESIZE = 10
     current_page = 1
-    
+
     # 屏蔽 sql alchemy 的 FSADeprecationWarning
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    
-    #支付单
+
+    # 支付单
     PAY_STATUS_DISPLAY_MAPPING = {
         "0": "待支付",
         "1": "支付成功",
@@ -30,7 +31,7 @@ class BaseConfig(object):
     PAY_REFUND_STATUS_DISPLAY_MAPPING = {
         "0": "正在退款",
         "1": "退款成功",
-        '-1':"退款失败"
+        '-1': "退款失败"
     }
 
 
@@ -46,30 +47,10 @@ class DevelopmentConfig(BaseConfig):
     # 插件模块暂时没有开启，以下配置可忽略
     # plugin config写在字典里面
     PLUGIN_PATH = {
-        'poem': {'path': 'app.plugins.poem', 'enable': True, 'version': '0.0.1', 'limit': 20},
-        'oss': {'path': 'app.plugins.oss', 'enable': True, 'version': '0.0.1', 'access_key_id': 'not complete',
-                'access_key_secret': 'not complete', 'endpoint': 'http://oss-cn-shenzhen.aliyuncs.com',
-                'bucket_name': 'not complete', 'upload_folder': 'app',
-                'allowed_extensions': ['jpg', 'gif', 'png', 'bmp']}
-    }
-
-
-class ProductionConfig(BaseConfig):
-    """
-    生产环境普通配置
-    """
-    DEBUG = False
-
-    # 令牌配置
-    JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=1)
-
-    # 插件模块暂时没有开启，以下配置可忽略
-    # plugin config写在字典里面
-    PLUGIN_PATH = {
-        'poem': {'path': 'app.plugins.poem', 'enable': True, 'version': '0.0.1', 'limit': 20},
-        'oss': {'path': 'app.plugins.oss', 'enable': True, 'version': '0.0.1', 'access_key_id': 'not complete',
-                'access_key_secret': 'not complete', 'endpoint': 'http://oss-cn-shenzhen.aliyuncs.com',
-                'bucket_name': 'not complete', 'upload_folder': 'app',
-                'allowed_extensions': ['jpg', 'gif', 'png', 'bmp']}
-    }
-
+    'goods': {'path': 'app.plugins.goods', 'enable': True, 'version': '0.1.0'},
+    'oss': {'path': 'app.plugins.oss', 'enable': True, 'version': '0.0.1', 'access_key_id': 'not complete', 'access_key_secret': 'not complete', 'endpoint': 'http://oss-cn-shenzhen.aliyuncs.com', 'bucket_name': 'not complete', 'upload_folder': 'app', 'allowed_extensions': ['jpg', 'gif', 'png', 'bmp']},
+    'wx': {'path': 'app.plugins.wx', 'enable': True, 'version': '0.1.0', 'wx_app': {'appid': 'wx37c9d625eddac654', 'app_secret': '8d11a05dc9127dd9cfd23e28e03bba2b', 'paykey': '02CBCE018C664DEB4462AB314B710193', 'mch_id': '1531633161', 'callback_url': 'http://flask.wkaanig.cn/plugin/wx/callback', 'refund_url': 'http://flask.wkaanig.cn/plugin/wx/rcallback'},
+    'pay_status_display_mapping': {'0': '待支付', '1': '支付成功', '2': '退款成功', '-1': '部分退款', '-2': '支付取消'},
+    'pay_refund_status_display_mapping': {'0': '正在退款', '1': '退款成功', '-1': '退款失败'}},
+    'poem': {'path': 'app.plugins.poem', 'enable': True, 'version': '0.0.1', 'limit': 20}
+}
